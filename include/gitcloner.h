ddde50870e239316c7876ac48842259f9efcbef7
MKM12345 <120132998+MKM12345@users.noreply.github.com>
2023-05-24 18:09:10 +0100

    Update gitcloner.h

--- include/gitcloner.h
+++ include/gitcloner.h
@@ -1,1 +1,10 @@
+#ifndef GIT_CLONER_H
+#define GIT_CLONER_H
+#include <string>
 
+class GitCloner {
+public:
+    static bool cloneRepository(const std::string& url, const std::string& path);
+};
+
+#endif  // GIT_CLONER_H
