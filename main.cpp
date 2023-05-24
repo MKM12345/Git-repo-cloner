19da7af98cbd3f1eafe8a5eb8d8015972c2c8bc6
MKM12345 <120132998+MKM12345@users.noreply.github.com>
2023-05-24 18:14:03 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,26 @@
+#include <iostream>
+#include "gitcloner.h"
 
+int main() {
+    // Prompt for repository URL
+    std::string url;
+    std::cout << "Enter the GitHub repository URL: ";
+    std::getline(std::cin, url);
+
+    // Prompt for local path
+    std::string path;
+    std::cout << "Enter the local path to clone the repository: ";
+    std::getline(std::cin, path);
+
+    // Clone repository
+    bool success = GitCloner::cloneRepository(url, path);
+
+    if (success) {
+       std::cout << "Repository cloned successfully!" << std::endl;
+    } else {
+       std::cout << "Error cloning repository." << std::endl;
+    }
+
+
+    return 0;
+}
