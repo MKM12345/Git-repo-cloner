91df929197332ff4a015395cb8649148923ad908
MKM12345 <120132998+MKM12345@users.noreply.github.com>
2023-05-24 18:09:49 +0100

    Update gitcloner.cpp

--- src/gitcloner.cpp
+++ src/gitcloner.cpp
@@ -1,1 +1,16 @@
+#include "gitcloner.h"
+#include <iostream>
+#include <cstdlib>
 
+bool GitCloner::cloneRepository(const std::string& url, const std::string& path) {
+    std::string gitCommand = "git clone " + url + " " + path;
+    int result = std::system(gitCommand.c_str());
+
+    if (result == 0) {
+        std::cout << "Repository cloned successfully!" << std::endl;
+        return true;
+    } else {
+        std::cout << "Error cloning repository." << std::endl;
+        return false;
+    }
+}
